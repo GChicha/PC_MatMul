dbf6736d515bd40ee0d3b0d3054d668224c8fa44
GChicha <guilherme.chichanoski@gmail.com>
2017-05-04 00:35:57 -0300

    Funcionando e tudo certo

--- mm_pthread.c
+++ mm_pthread.c
@@ -1,39 +1,41 @@
+#ifndef SEQ
 #include <pthread.h>
+#endif
+
 #include <stdlib.h>
+
+#ifdef DEBUG
 #include <stdio.h>
+#endif
 
 #define MAX 5
 #define MIN 1
 
-#ifndef N_THREAD
 #ifdef SEQ
 #define N_THREAD 1
 #else
+#ifndef N_THREAD
 #define N_THREAD 8
 #endif
 #endif
 
 struct thread_start {
-    int **matriz1, **matriz2;
-    int i,j;
+    int **matriz1, **matriz2, **result;
+    int inicio;
+    int fim;
     int n;
-    int *result;
 };
 
 void *multi(void *x) {
     struct thread_start *vars = (struct thread_start*)x;
 
-    int v[vars->n];
-    for (int i = 0; i < vars->n; i++) {
-        v[i] = vars->matriz2[i][vars->j];
+    for (int i = vars->inicio; i < vars->fim; i++) {
+        for (int j = 0; j < vars->n; j++) {
+            for (int k = 0; k < vars->n; k++) {
+                vars->result[i][j] += vars->matriz1[i][k] * vars->matriz2[k][j];
+            }
+        }
     }
-
-    int soma = 0;
-    for (int i = 0; i < vars->n; i++) {
-        soma += vars->matriz1[vars->i][i] * v[i];
-    }
-
-    *vars->result = soma;
 
     free(vars);
     return NULL;
@@ -48,6 +50,7 @@ int main(int argc, char const *argv[]) {
     int **matriz2 = (int **)calloc(n, sizeof(int*));
 
     int **result = (int **)calloc(n, sizeof(int*));
+
     #ifdef DEBUG
     printf("Matriz1:\n");
     #endif
@@ -80,33 +83,34 @@ int main(int argc, char const *argv[]) {
     printf("\n");
     #endif
 
-    pthread_t *vet_thread = (pthread_t *)calloc(n*n, sizeof(pthread_t));
+    #ifndef SEQ
+    pthread_t *vet_thread = (pthread_t *)calloc(N_THREAD, sizeof(pthread_t));
+    #endif
 
     struct thread_start *vars = NULL;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            vars = (struct thread_start*)calloc(1, sizeof(struct thread_start));
-            vars->matriz1 = matriz1;
-            vars->matriz2 = matriz2;
-            vars->i = i;
-            vars->j = j;
-            vars->n = n;
-            vars->result = &result[i][j];
-            #ifdef SEQ
-            multi(vars);
-            #else
-            pthread_create(&vet_thread[j + i*n], NULL, multi, vars);
-            #endif
-        }
+    for (int i = 0; i < N_THREAD; i++) {
+        vars = (struct thread_start*)calloc(1, sizeof(struct thread_start));
+        vars->matriz1 = matriz1;
+        vars->matriz2 = matriz2;
+        vars->result = result;
+        vars->inicio = (i*n)/N_THREAD;
+        vars->fim = ((i+1)*n)/N_THREAD;
+        vars->n = n;
+        #ifdef SEQ
+        multi(vars);
+        #else
+        pthread_create(&vet_thread[i], NULL, multi, vars);
+        #endif
     }
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            pthread_join(vet_thread[j + i*n], NULL);
-        }
+    #ifndef SEQ
+    for (int i = 0; i < N_THREAD; i++) {
+        pthread_join(vet_thread[i], NULL);
     }
     free(vet_thread);
+    #endif
+
     #ifdef DEBUG
     printf("Resultado:\n");
 
